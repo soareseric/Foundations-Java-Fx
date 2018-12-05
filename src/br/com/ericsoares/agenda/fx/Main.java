25c9645f1abf57a6a1c941a9cab9d37cc6b81f29
soareseric <contato.eric.sm@gmail.com>
2018-12-05 14:20:08 -0200

    Definicoes basicas

--- src/br/com/ericsoares/agenda/fx/Main.java
+++ src/br/com/ericsoares/agenda/fx/Main.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -15,6 +16,8 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Minha agenda");
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
